4711438228f28d0cd41d3f3ca69e3878546e5277
stw8194 <stw8194@gmail.com>
2024-07-23 14:42:53 +0900

    test: verified behavior when modifying an existing exchange rate

--- js/test_money.js
+++ js/test_money.js
@@ -4,12 +4,12 @@ const Portfolio = require('./portfolio');
 const Bank = require('./bank')
 
 class MoneyTest {
-    constructor() {
+    setUp() {
         this.bank = new Bank();
         this.bank.addExchangeRate("EUR", "USD", 1.2);
         this.bank.addExchangeRate("USD", "KRW", 1100);
     }
-
+    
     getAllTestMethods() {
         let moneyPrototype = MoneyTest.prototype;
         let allProps = Object.getOwnPropertyNames(moneyPrototype);
@@ -39,24 +39,6 @@ class MoneyTest {
         let portfolio = new Portfolio();
         portfolio.add(fiveDollars, tenDollars);
         assert.deepStrictEqual(portfolio.evaluate(this.bank,"USD"), fifteenDollars);
-    }
-
-    runAllTest() {
-        let testMethods = this.getAllTestMethods();
-        testMethods.forEach(m => {
-            console.log("Running: %s()", m);
-            let method = Reflect.get(this, m);
-            try {
-                Reflect.apply(method, this, []);
-            } catch (e) {
-                if (e instanceof assert.AssertionError) {
-                    console.log(e);
-                } else {
-                    throw e;
-                }
-            }
-            
-        });
     }
 
     testAdditionOfDollarsAndEuros() {
@@ -95,20 +77,48 @@ class MoneyTest {
         // 화살표 함수를 사용하면 this가 상위함수 스코프에 바인딩
     }
 
-    testConversion() {
-        let bank = new Bank();
-        bank.addExchangeRate("EUR", "USD", 1.2);
+    testConversionWithDifferentRatesBetweenTwoCurrencies() {
         let tenEuros = new Money(10, "EUR");
         assert.deepStrictEqual(
-            bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+            this.bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+        this.bank.addExchangeRate("EUR", "USD", 1.3)
+        assert.deepStrictEqual(
+            this.bank.convert(tenEuros, "USD"), new Money(13, "USD"));
     }
 
     testConversionWithMissingExchangeRate() {
-        let bank = new Bank();
         let tenEuros = new Money(10, "EUR");
         let expectedError = new Error("EUR->Kalganid");
-        assert.throws(function () {bank.convert(tenEuros, "Kalganid")},
+        assert.throws(() => {this.bank.convert(tenEuros, "Kalganid")},
             expectedError);
+    }
+    
+    // 한 테스트가 다른 테스트에 미치는 원치 않는 부작용 확인
+    // randomizeTestOrder(testMethods) {
+    //     for (let i = testMethods.length -1; i > 0; i--) {
+    //         const j = Math.floor(Math.random() * (i + 1));
+    //         [testMethods[i], testMethods[j]] = [testMethods[j], testMethods[i]];
+    //     }
+    //     return testMethods;
+    // }
+
+    runAllTest() {
+        let testMethods = this.getAllTestMethods();
+        testMethods.forEach(m => {
+            console.log("Running: %s()", m);
+            let method = Reflect.get(this, m);
+            try {
+                this.setUp();
+                Reflect.apply(method, this, []);
+            } catch (e) {
+                if (e instanceof assert.AssertionError) {
+                    console.log(e);
+                } else {
+                    throw e;
+                }
+            }
+            
+        });
     }
 }
 
